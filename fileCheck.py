54f176b92a74d8040cb88b50099f54524549034c
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-10 17:29:28 -0500

    filecheck

---
+++ fileCheck.py
@@ -0,0 +1,50 @@
+# modules
+import os
+import glob
+import time
+
+# Iterate through all files in directory
+fPath = 'C:\\Users\\Miles\\Documents\\GitHub\\Advanced-Python-Projects\\fileCheck\\'
+
+dir_list = os.listdir(fPath) 
+  
+print("Files in '", fPath, "':\n")  
+  
+print(dir_list)
+
+print("\n")
+
+# Concatenate file name and path
+fName = 'animalList.txt'
+
+abPath = os.path.join(fPath, fName)
+
+# Gather all .txt files in list, convert individual list elements to a string path,
+# use that path to find when last modified 
+os.chdir(fPath)
+txtFiles = glob.glob('*.txt')
+
+print("List of .txt files and when they were modified:\n")
+
+i = 0
+while i < len(txtFiles):
+    txtFileString = "".join(txtFiles[i])
+
+    txtFilePath = fPath + txtFileString
+
+    modTimesinceEpoc = os.path.getmtime(txtFilePath)
+    modificationTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modTimesinceEpoc))
+ 
+    print(txtFileString + " was last modified on " + modificationTime)
+
+    i += 1
+
+
+
+    
+
+
+
+
+
+
